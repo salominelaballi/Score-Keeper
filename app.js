9a1df245f4764723fb8dc8d296459968dc34b295
Salomi <salomisindhu.33@gmail.com>
2021-08-12 10:31:32 -0400

    add functionality

---
+++ app.js
@@ -0,0 +1,41 @@
+const p1button = document.querySelector('#p1button');
+const p2button = document.querySelector('#p2button');
+const resetbutton = document.querySelector('#reset')
+
+const p1Display = document.querySelector('#p1Display')
+const p2Display = document.querySelector('#p2Display')
+
+let p1Score = 0;
+let p2Score = 0;
+let winningScore = 5;
+let isGameOver = false;
+
+p1button.addEventListener('click', function () {
+    if (!isGameOver) {
+        p1Score += 1
+        if (p1Score === winningScore) {
+            isGameOver = true;
+        }
+
+    }
+    p1Display.innerText = p1Score
+
+})
+
+p2button.addEventListener('click', function () {
+    if (!isGameOver) {
+        p2Score += 1
+        if (p2Score === winningScore) {
+            isGameOver = true;
+        }
+
+    }
+    p2Display.innerText = p2Score
+
+})
+resetbutton.addEventListener('click', function () {
+    p1Score = 0;
+    p2Score = 0;
+    p1Display.innerText = p1Score
+    p2Display.innerText = p2Score
+})
